25858d91d2c9e80736de7ba8a3a9a0dcf2e61aba
Cristian Sanchez Pelaez <1006108728@academico.icesi.edu.co>
2019-11-26 12:31:38 -0500

    added contracts for graph interface

--- src/structures/Graph.java
+++ src/structures/Graph.java
@@ -60,26 +60,77 @@ public interface Graph<V> {
 	 */
 	public boolean areConnected(V v, V u);
 	
+	/**
+	 * Tells if the graph is directed
+	 * @return true/false if the graph is directed or not
+	 */
 	public boolean isDirected();
 	
+	/**
+	 * Calls to the bfs algorithm in order to find paths between vertices
+	 * @param v initial vertex
+	 * @return List of all the vertices
+	 */
 	public List<V> bfs(V v);
 	
+	/**
+	 * Calls to the dfs algorithm in order to find paths between vertices
+	 * @param v initial vertex
+	 * @return List of all the vertices
+	 */
 	public List<V> dfs(V v);
 	
+	/**
+	 * Calls the dijkstra algorithm in order to get the shortest paths from one vertex to all the others
+	 * @param v initial vertex
+	 * @return array with the shortest paths from the initial vertex to the others
+	 */
 	public int[] dijkstra(V v);
 	
+	/**
+	 * calls the floyd-warshall algorithm to get the shortest paths from and to any vertex
+	 * @return matrix with the shortest paths
+	 */
 	public int[][] floydWarshall();
 	
+	/**
+	 * Calls the Kruskal algorithm to create the shortest path possible that connects all the vertices possibles for a possible disconnected graph
+	 * @param p Weighted matrix
+	 * @return Matrix showing the connections of the MST
+	 */
 	public int[][] Kruskal(int[][] p);
 	
+	/**
+	 * Calls the Prim algorithm the generate a MST for a graph without disconeccions
+	 * @param p Weighted matrix
+	 * @return Array of the weight of the MST
+	 */
 	public int[] Prim(int[][] p);
 	
+	/**
+	 * The method gives the amount of vertices in the graph
+	 * @return amount of vertices
+	 */
 	public int getVertexSize();
 	
+	/**
+	 * The method gives the map of the vertices with their respective position
+	 * @return map with vertices and their position
+	 */
 	public Map<V, Integer> getVertices();
-
+	
+	/**
+	 * Gives the position of a vertex
+	 * @param vertex
+	 * @return position of the vertex
+	 */
 	public int getIndex(V vertex);
-
+	
+	/**
+	 * Gives all the adjacent vertex of a specified vertex
+	 * @param vertex specified vertex
+	 * @return List with all the vertices that have direct connection with vertex
+	 */
 	public List<V> vertexAdjacent(V vertex);
 	
 }
